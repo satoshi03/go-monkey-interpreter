8947e79b57db31dbe1abffe4f81785344729947a
Satoshi Innami <innamisatoshi@gmail.com>
2019-01-06 17:49:43 +0900

    add parser to repl

--- repl/repl.go
+++ repl/repl.go
@@ -6,10 +6,19 @@ import (
 	"io"
 
 	"github.com/satoshi03/go-monkey-interpreter/lexer"
-	"github.com/satoshi03/go-monkey-interpreter/token"
+	"github.com/satoshi03/go-monkey-interpreter/parser"
 )
 
 const PROMPT = ">> "
+const MONKEY = `
+w  c(..)o   (
+ \__(-)    __)
+     /\   (
+    /(_)___)
+   w /|
+    | \
+    m  m
+`
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -22,9 +31,24 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, MONKEY)
+		io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+		io.WriteString(out, " parser errors:\n")
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
