63e5d88324ac9d7230d3a3f16f6f217b071997db
Satoshi Innami <innamisatoshi@gmail.com>
2019-02-03 18:46:44 +0900

    add evaluator test

---
+++ evaluator/evaluator_test.go
@@ -0,0 +1,48 @@
+package evaluator
+
+import (
+	"testing"
+
+	"github.com/satoshi03/go-monkey-interpreter/lexer"
+	"github.com/satoshi03/go-monkey-interpreter/object"
+	"github.com/satoshi03/go-monkey-interpreter/parser"
+)
+
+func testEvalIntegerExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+func testEval(input string) object.Object {
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParserProgram()
+
+	return Eval(program)
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	result, ok := obj.(*object.Integer)
+	if !ok {
+		t.Errorf("object is not Integer. get=%d (%+v)", obj, obj)
+		return false
+	}
+
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%d, want=%d",
+			result.Value, expected)
+		return false
+	}
+
+	return true
+}
